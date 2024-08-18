3d6f39e6dcc527fac1a79bd7fcc020a7813fd4dc
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-08-18 19:03:46 +0300

    Added team display

--- views/home.view.php
+++ views/home.view.php
@@ -38,7 +38,6 @@
     </nav>
     <!-- Body -->
     <!-- Hero Section -->
-
     <div
         class="sm:container mx-4 sm:mx-auto mt-8 flex flex-col-reverse sm:flex-row items-center justify-center sm:items-stretch sm:justify-between p-6 sm:p-8 bg-white dark:bg-gray-900 shadow-lg rounded-lg">
         <!-- Left Side: Welcome Message and User Info -->
@@ -68,6 +67,52 @@
         </div>
     </div>
 
+    <!-- Show Users -->
+    <section class="bg-white dark:bg-gray-800">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+            <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Manage team</h2>
+                <ev class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Preview current registered users,
+                    update, or remove them.</ev>
+            </div>
+            <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <div class="text-center text-gray-500 dark:text-gray-400">
+                    <img class="mx-auto mb-4 w-36 h-36 rounded-full"
+                        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                        alt="Bonnie Avatar">
+                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bonnie Green</h3>
+                    <p>Email: <span class="text-blue-500">boonie.green@example.com</span></p>
+                    <div class="mt-4 max-w-60 mx-auto flex justify-center gap-1">
+                        <button class="default-btn flex-1">Edit</button>
+                        <button class="danger-btn flex-1">Remove</button>
+                    </div>
+                </div>
+                <div class="text-center text-gray-500 dark:text-gray-400">
+                    <img class="mx-auto mb-4 w-36 h-36 rounded-full"
+                        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/helene-engels.png"
+                        alt="Helene Avatar">
+                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Helene Engels</h3>
+                    <p>Email: <span class="text-blue-500">helene.engels@example.com</span></p>
+                    <div class="mt-4 max-w-60 mx-auto flex justify-center gap-1">
+                        <button class="default-btn flex-1">Edit</button>
+                        <button class="danger-btn flex-1">Remove</button>
+                    </div>
+                </div>
+                <div class="text-center text-gray-500 dark:text-gray-400">
+                    <img class="mx-auto mb-4 w-36 h-36 rounded-full"
+                        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                        alt="Jese Avatar">
+                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jese Leos</h3>
+                    <p>Email: <span class="text-blue-500">jese.leos@example.com</span></p>
+                    <div class="mt-4 max-w-60 mx-auto flex justify-center gap-1">
+                        <button class="default-btn flex-1">Edit</button>
+                        <button class="danger-btn flex-1">Remove</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
     <script src=" /node_modules/flowbite/dist/flowbite.js"></script>
 </body>
 
